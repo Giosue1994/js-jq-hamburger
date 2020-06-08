1df80e79005f204748b4df442b150633e14a5100
Giosue1994 <giosuelentini1994@outlook.com>
2020-06-09 01:07:29 +0200

    modifica selettore

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+// aggiungo una classe all'ultimo tag 'a' che sta dentro un div
+$('.header-right a:last').addClass('open');
+
 // quando clicco sull'icona hamburger compare il menu
-$('.fa-bars').click(
+$('.open').click(
   function() {
     $('.hamburger-menu').addClass('active');
   }
 );
 
-// quando clicco sulli icona x scompare il menu
+// quando clicco sull' icona x scompare il menu
 $('.close').click(
   function() {
     $('.hamburger-menu').removeClass('active');
