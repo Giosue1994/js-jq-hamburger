0f4f50eadcf2f9541a376b45dff0063f0ef4d62f
Giosue1994 <giosuelentini1994@outlook.com>
2020-06-08 15:49:30 +0200

    hamburger menu add/remove class

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // quando clicco sull'icona hamburger compare il menu
 $('.fa-bars').click(
   function() {
-    $('.hamburger-menu').fadeIn();
+    $('.hamburger-menu').addClass('active');
   }
 );
 
 // quando clicco sulli icona x scompare il menu
 $('.close').click(
   function() {
-    $('.hamburger-menu').fadeOut();
+    $('.hamburger-menu').removeClass('active');
   }
 );
